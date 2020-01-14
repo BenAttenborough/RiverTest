84016350f2709c3654fb7bbf13b213ad41cafb18
Ben Attenborough <battenborough@gmail.com>
2020-01-14 14:48:51 +0000

    Attempt to use frontend to get data from backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function Contract() {
+  const [data, setData] = useState(null);
+
+  let url = "http://localhost:9000/data";
+
+  useEffect(() => {
+    fetch(url)
+      .then(resp => {
+        console.log(resp);
+        return resp.json();
+      })
+      .then(info => {
+        console.log(info);
+        setData(info);
+      });
+  }, []);
+
+  return <div className="Contract">{data ? <p>data</p> : <p>No data</p>}</div>;
+}
 
 function App() {
   return (
@@ -10,6 +30,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Contract />
         <a
           className="App-link"
           href="https://reactjs.org"
