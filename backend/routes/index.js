1f3f93d1a41afd5f1e5dfeefc6007321a38d3b35
Ben Attenborough <battenborough@gmail.com>
2020-01-14 14:39:35 +0000

    Test sending data via /data route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,30 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const contract = {
+  data: {
+    type: "contracts",
+    id: "dc89ff49-8449-11e7-ac1d-3c52820efd20",
+    attributes: {
+      name: "Contract Name"
+    }
+  },
+  relationships: {
+    paragraphs: {
+      links: {
+        self: "/contracts/dc89ff49-8449-11e7-ac1d-3c52820efd20/paragraphs"
+      }
+    }
+  }
+};
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/data", function(req, res, next) {
+  res.send(contract);
 });
 
 module.exports = router;
