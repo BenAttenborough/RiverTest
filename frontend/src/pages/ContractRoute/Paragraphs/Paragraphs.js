fc67e2f024c4a9fb3faed67cbe0be515459e66b3
Ben Attenborough <battenborough@gmail.com>
2020-01-17 15:26:23 +0000

    Refactor fetching of paragraphs

--- frontend/src/pages/ContractRoute/Paragraphs/Paragraphs.js
+++ frontend/src/pages/ContractRoute/Paragraphs/Paragraphs.js
@@ -13,7 +13,8 @@ function Button(props) {
 
 function fetchPars(fetchParams) {
   console.log("Fetching pars");
-  const { id, currentPage, pageSize, setData } = fetchParams;
+  let { id, currentPage, pageSize, setContent, content } = fetchParams;
+
   const url = `http://localhost:4000/contract/${id}/paragraphs?page=${currentPage}&pageSize=${pageSize}`;
   fetch(url)
     .then(handleErrors)
@@ -22,48 +23,54 @@ function fetchPars(fetchParams) {
       return resp.json();
     })
     .then(info => {
-      console.log("info >>>>>", info);
-      console.log("info >>>>>", info.content);
-      // If there is data merge the new info with it
-      // if (data) {
-      //   let oldContent = data.content.slice();
-      //   let newContent = info.content.slice();
-      //   // console.log("oldContent", oldContent);
-      //   // console.log("newContent", newContent);
-      //   let combinedData = oldContent.concat(newContent);
-      //   // console.log("combinedData", combinedData);
-      //   setData({
-      //     content: combinedData,
-      //     page: info.page,
-      //     pageSize: info.pageSize
-      //   });
-      // }
-      setData(info);
+      let formattedContent = info.content.map(item => {
+        return {
+          id: item.id,
+          text: item.attributes.text
+        };
+      });
+      // formattedContent = [
+      //   ...formattedContent,
+      //   {
+      //     id: "fooo",
+      //     text: "Extra line"
+      //   }
+      // ];
+      // console.log("formattedContent", formattedContent);
+      if (content) {
+        console.log("content -->", content);
+        formattedContent = [...content, ...formattedContent];
+      }
+      console.log("formattedContent", formattedContent);
+
+      setContent(formattedContent);
     })
     .catch(err => {
       console.log(err);
-      setData({ error: err });
+      setContent({ error: err });
     });
 }
 
 function ShowParagraphs(props) {
-  const { data } = props;
-  if (!data.content) {
-    return <p>There is no data for specified page</p>;
+  const { content } = props;
+  if (!content) {
+    return <p>There is no content for specified page</p>;
   }
-  return data.content.map(item => {
-    return <p key={item.id}>{item.attributes.text}</p>;
+  return content.map(item => {
+    return <p key={item.id}>{item.text}</p>;
   });
 }
 
-function ShowContent(data) {
+function ShowContent(props) {
+  const { content, currentPage, setCurrentPage } = props;
   return (
     <div>
-      <ShowParagraphs data={data} />
+      <ShowParagraphs content={content} />
       <Button
         label={"Fetch more paragraphs"}
         action={() => {
-          console.log("x");
+          console.log("currentPage,", currentPage);
+          setCurrentPage(currentPage + 1);
         }}
       />
     </div>
@@ -73,27 +80,36 @@ function ShowContent(data) {
 export function Paragraphs(props) {
   let { page, pageSize } = props;
   let { id } = useParams();
-  const [data, setData] = useState(null);
+  const [content, setContent] = useState(null);
   const [currentPage, setCurrentPage] = useState(parseInt(page));
   const fetchParams = {
     id,
     currentPage,
     pageSize,
-    setData
+    setContent,
+    content
   };
   // let url = `http://localhost:4000/contract/${id}/paragraphs?page=${page}&pageSize=${pageSize}`;
 
   useEffect(() => {
     fetchPars(fetchParams);
-  }, [page]);
-  console.log("data >>>>>>", data);
-  console.log("page", page);
+  }, [currentPage]);
+  // console.log("content >>>>>>", content);
+  // console.log("page", page);
   return (
     <div>
       <p>Paras route...</p>
       <p>{`page: ${page}`}</p>
       <p>{`pageSize: ${pageSize}`}</p>
-      {data ? ShowContent(data) : "Fetching data"}
+      {content ? (
+        <ShowContent
+          content={content}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+        />
+      ) : (
+        "Fetching data"
+      )}
     </div>
   );
 }
