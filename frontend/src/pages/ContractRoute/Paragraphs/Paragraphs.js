3aa0dbe0daf2ad8c11111277aacfa56bc05e6316
Ben Attenborough <battenborough@gmail.com>
2020-01-17 12:50:46 +0000

    Refactor fetching of paragraphs

--- frontend/src/pages/ContractRoute/Paragraphs/Paragraphs.js
+++ frontend/src/pages/ContractRoute/Paragraphs/Paragraphs.js
@@ -11,8 +11,39 @@ function Button(props) {
   );
 }
 
-function fetchPars() {
+function fetchPars(fetchParams) {
   console.log("Fetching pars");
+  const { id, currentPage, pageSize, setData } = fetchParams;
+  const url = `http://localhost:4000/contract/${id}/paragraphs?page=${currentPage}&pageSize=${pageSize}`;
+  fetch(url)
+    .then(handleErrors)
+    .then(resp => {
+      console.log(resp);
+      return resp.json();
+    })
+    .then(info => {
+      console.log("info >>>>>", info);
+      console.log("info >>>>>", info.content);
+      // If there is data merge the new info with it
+      // if (data) {
+      //   let oldContent = data.content.slice();
+      //   let newContent = info.content.slice();
+      //   // console.log("oldContent", oldContent);
+      //   // console.log("newContent", newContent);
+      //   let combinedData = oldContent.concat(newContent);
+      //   // console.log("combinedData", combinedData);
+      //   setData({
+      //     content: combinedData,
+      //     page: info.page,
+      //     pageSize: info.pageSize
+      //   });
+      // }
+      setData(info);
+    })
+    .catch(err => {
+      console.log(err);
+      setData({ error: err });
+    });
 }
 
 function ShowParagraphs(props) {
@@ -29,48 +60,31 @@ function ShowContent(data) {
   return (
     <div>
       <ShowParagraphs data={data} />
-      <Button label={"Fetch more paragraphs"} action={fetchPars} />
+      <Button
+        label={"Fetch more paragraphs"}
+        action={() => {
+          console.log("x");
+        }}
+      />
     </div>
   );
 }
 
 export function Paragraphs(props) {
-  let { pageSize } = props;
+  let { page, pageSize } = props;
   let { id } = useParams();
   const [data, setData] = useState(null);
-  const [page, setPage] = useState(parseInt(props.page));
-  let url = `http://localhost:4000/contract/${id}/paragraphs?page=${page}&pageSize=${pageSize}`;
+  const [currentPage, setCurrentPage] = useState(parseInt(page));
+  const fetchParams = {
+    id,
+    currentPage,
+    pageSize,
+    setData
+  };
+  // let url = `http://localhost:4000/contract/${id}/paragraphs?page=${page}&pageSize=${pageSize}`;
 
   useEffect(() => {
-    fetch(url)
-      .then(handleErrors)
-      .then(resp => {
-        console.log(resp);
-        return resp.json();
-      })
-      .then(info => {
-        console.log("info >>>>>", info);
-        console.log("info >>>>>", info.content);
-        // If there is data merge the new info with it
-        if (data) {
-          let oldContent = data.content.slice();
-          let newContent = info.content.slice();
-          // console.log("oldContent", oldContent);
-          // console.log("newContent", newContent);
-          let combinedData = oldContent.concat(newContent);
-          // console.log("combinedData", combinedData);
-          setData({
-            content: combinedData,
-            page: info.page,
-            pageSize: info.pageSize
-          });
-        }
-        setData(info);
-      })
-      .catch(err => {
-        console.log(err);
-        setData({ error: err });
-      });
+    fetchPars(fetchParams);
   }, [page]);
   console.log("data >>>>>>", data);
   console.log("page", page);
