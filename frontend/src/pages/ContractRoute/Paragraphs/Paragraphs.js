acff12e577f7dd55a60d5d8fac658217e23e208b
Ben Attenborough <battenborough@gmail.com>
2020-01-18 23:35:59 +0000

    Refactor Paragraphs and split out functionality

--- frontend/src/pages/ContractRoute/Paragraphs/Paragraphs.js
+++ frontend/src/pages/ContractRoute/Paragraphs/Paragraphs.js
@@ -1,168 +1,12 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { makeFetchRequest } from "../../../utils/utils";
-import { baseUrl } from "../../../settings/constants";
+import { fetchPars, fetchTitle, ShowContent } from "./pagagraphFunctionality";
 
-function Button(props) {
-  const { label, action } = props;
-  return (
-    <div className="button" onClick={action}>
-      <span>{label}</span>
-    </div>
-  );
-}
-
-function fetchPars({
-  id,
-  currentPage,
-  pageSize,
-  setContent,
-  prevContent,
-  setEOF
-}) {
-  const url = `${baseUrl}/contract/${id}/paragraphs?page=${currentPage}&pageSize=${pageSize}`;
-
-  makeFetchRequest(url, data => {
-    updateParagraphs(data, prevContent, setContent, setEOF);
-  });
-}
-
-function updateParagraphs(data, prevContent, setContent, setEOF) {
-  let formattedContent = [];
-  if (data.content) {
-    formattedContent = data.content.map(item => {
-      return {
-        id: item.id,
-        text: item.attributes.text
-      };
-    });
-  }
-  if (prevContent) {
-    formattedContent = prevContent.concat(formattedContent);
-  }
-  const lastItem = formattedContent[formattedContent.length - 1];
-  if (lastItem.id === "EOF") {
-    setEOF(true);
-  }
-  // console.log("formattedContent", formattedContent);
-  // console.log("formattedContent.length", formattedContent.length);
-  // console.log("lastitem", formattedContent[formattedContent.length - 1]);
-  setContent(formattedContent);
-}
-
-function ShowParagraphs(props) {
-  const { content } = props;
-  // console.log("xContentx:", content);
-  if (!content) {
-    return <li>There is no content for specified page</li>;
-  }
-  return content.map(item => {
-    return <li key={item.id}>{item.text}</li>;
-  });
-}
-
-function onScroll() {
-  console.log("Scrolled to bottom");
-}
-
-function useVis(
-  ref,
-  currentPage,
-  setCurrentPage,
-  fetchParams,
-  prevContent,
-  content,
-  EOF
-) {
-  console.log("useVis prevContent", prevContent);
-
-  const observer = new IntersectionObserver(
-    ([entry]) => {
-      if (entry.intersectionRatio === 1) {
-        console.log("useVis prevContent inner", prevContent);
-        console.log("fetchParams.currentPage", fetchParams.currentPage);
-        console.log("currentPage", currentPage);
-
-        fetchParams = {
-          ...fetchParams,
-          currentPage: fetchParams.currentPage + 1,
-          prevContent: content
-        };
-        console.log("Updated fetchParams", fetchParams);
-        console.log("********* EOF:", EOF);
-
-        fetchPars(fetchParams);
-        setCurrentPage(currentPage + 1);
-        // *** setEOF
-      }
-    },
-    {
-      root: null,
-      rootMargin: "0px",
-      threshold: 1.0
-    }
-  );
-
-  // return observer;
-  useEffect(() => {
-    if (!EOF) {
-      console.log("triggered useEffect");
-      // console.log("ref.current", ref.current);
-      // console.log("triggered:", triggered);
-      if (ref.current) {
-        console.log("ref.current is true");
-        observer.observe(ref.current);
-      }
-      return () => {
-        console.log("Cleaning up useEffect observer");
-        observer.disconnect();
-      };
-    }
-  }, [content]);
-}
-
-function ShowContent(props) {
-  const {
-    content,
-    currentPage,
-    setCurrentPage,
-    EOF,
-    fetchParams,
-    prevContent
-  } = props;
-  console.log("prevContent", prevContent);
-  console.log("content", content);
-  const scrollEle = useRef(null);
-
-  useVis(
-    scrollEle,
-    currentPage,
-    setCurrentPage,
-    fetchParams,
-    prevContent,
-    content,
-    EOF
-  );
-
-  return (
-    <div>
-      <ul className="contract-paragraphs">
-        <ShowParagraphs content={content} />
-      </ul>
-      {!EOF && (
-        <Button
-          label={"Fetch more paragraphs"}
-          action={() => {
-            console.log("currentPage,", currentPage);
-            setCurrentPage(currentPage + 1);
-          }}
-        />
-      )}
-      {!EOF && <div ref={scrollEle}>---Scroll for more---</div>}
-    </div>
-  );
-}
-
+/**
+ * Component that wraps all the page functionality together
+ *
+ * @param {object} props
+ */
 export function Paragraphs(props) {
   let { page, pageSize } = props;
   // Defaults if parameters missing
@@ -187,16 +31,11 @@ export function Paragraphs(props) {
     setEOF
   };
 
-  const url = `http://localhost:4000/contract/${id}`;
-
-  // Fetch paragraphs on component mount and then every time currentPage is incremented
+  // Fetch some paragraphs and title once on component mount
   useEffect(() => {
     if (!EOF) {
-      console.log("Fecthing new paragraphs: ", fetchParams);
       fetchPars(fetchParams);
-      makeFetchRequest(url, meta => {
-        setTitle(meta.data.attributes.name);
-      });
+      fetchTitle(id, setTitle);
     }
   }, []);
 
@@ -207,30 +46,19 @@ export function Paragraphs(props) {
       <p>{`Fetching ${pageSize} paragraphs at a time`}</p>
       {content ? (
         content.error ? (
-          <Error message={content.error} />
+          <p>{content.error}</p>
         ) : (
-          <div>
-            <ShowContent
-              content={content}
-              currentPage={currentPage}
-              setCurrentPage={setCurrentPage}
-              EOF={EOF}
-              fetchParams={fetchParams}
-              prevContent={content}
-            />
-          </div>
+          <ShowContent
+            content={content}
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+            EOF={EOF}
+            fetchParams={fetchParams}
+          />
         )
       ) : (
         "Fetching data"
       )}
-    </div>
-  );
-}
-
-function Error({ message }) {
-  return (
-    <div>
-      <p>{`${message}`}</p>
     </div>
   );
 }
---
+++ frontend/src/pages/ContractRoute/Paragraphs/pagagraphFunctionality.js
@@ -0,0 +1,162 @@
+import React, { useEffect, useRef } from "react";
+import { makeFetchRequest } from "../../../utils/utils";
+import { baseUrl } from "../../../settings/constants";
+function Button(props) {
+  const { label, action } = props;
+  return (
+    <div className="button" onClick={action}>
+      <span>{label}</span>
+    </div>
+  );
+}
+/**
+ * Fetches the contract title and sets it
+ *
+ * @param {int} id
+ * @param {function} setTitle
+ */
+export function fetchTitle(id, setTitle) {
+  const url = `${baseUrl}/contract/${id}`;
+  makeFetchRequest(url, response => {
+    setTitle(response.data.attributes.name);
+  });
+}
+/**
+ * Fetches paragraphs based on:
+ * id: The id of the contract
+ * currentPage: The "page" of paragraphs to retrieve (defaults to 1)
+ * pageSize: The number of paragraphs to retrieve per page
+ *
+ * setContent: A function which sets the content state
+ * prevContent: The previous paragraphs fetched, if any. New paragraphs will be added to the end
+ * setEOF: If the end of the contract is his this will be called to inform the system to stop fetching paragraphs
+ *
+ * @param {object} param0
+ */
+export function fetchPars({
+  id,
+  currentPage,
+  pageSize,
+  setContent,
+  prevContent,
+  setEOF
+}) {
+  const url = `${baseUrl}/contract/${id}/paragraphs?page=${currentPage}&pageSize=${pageSize}`;
+  makeFetchRequest(url, data => {
+    updateParagraphs(data, prevContent, setContent, setEOF);
+  });
+}
+/**
+ * Updates the content state with additional paragraphs
+ *
+ * @param {object} data The data where the paragraphs reside
+ * @param {object} prevContent The previous paragraphs fetched, if any. New paragraphs will be added to the end
+ * @param {function} setContent A function which sets the content state
+ * @param {function} setEOF If the end of the contract is hit this will be called to inform the system to stop fetching paragraphs
+ */
+function updateParagraphs(data, prevContent, setContent, setEOF) {
+  let formattedContent = [];
+  if (data.content) {
+    formattedContent = data.content.map(item => {
+      return {
+        id: item.id,
+        text: item.attributes.text
+      };
+    });
+  }
+  if (prevContent) {
+    formattedContent = prevContent.concat(formattedContent);
+  }
+  const lastItem = formattedContent[formattedContent.length - 1];
+  if (lastItem.id === "EOF") {
+    setEOF(true);
+  }
+  setContent(formattedContent);
+}
+/**
+ * Shows all the paragraphs sent to it
+ * NOTE: Every paragraph must have a unique key
+ *
+ * @param { object } props Contains props.content
+ */
+function ShowParagraphs({ content }) {
+  // console.log("xContentx:", content);
+  if (!content) {
+    return <li>There is no content for specified page</li>;
+  }
+  return content.map(item => {
+    return <li key={item.id}>{item.text}</li>;
+  });
+}
+/**
+ * Watches for when user scrolls to the bottom of the page
+ * and fetches additional paragraphs (if any).
+ * It does this by watch for when the element indicated by ref
+ * appears in the viewport
+ *
+ * @param {object} ref A reference to the element which will trigger the fetch
+ * @param {int} currentPage The current page of paragraphs shown
+ * @param {function} setCurrentPage Function that sets the page state
+ * @param {object} fetchParams Params for fetching new paragraphs
+ * @param {array} content The current paragraphs displayed
+ * @param {boolean} EOF Indicates if we have hit the end of the file
+ */
+function useVis(ref, currentPage, setCurrentPage, fetchParams, content, EOF) {
+  const observer = new IntersectionObserver(
+    ([entry]) => {
+      if (entry.intersectionRatio === 1) {
+        fetchParams = {
+          ...fetchParams,
+          currentPage: fetchParams.currentPage + 1,
+          prevContent: content
+        };
+        fetchPars(fetchParams);
+        setCurrentPage(currentPage + 1);
+      }
+    },
+    {
+      root: null,
+      rootMargin: "0px",
+      threshold: 1.0
+    }
+  );
+  useEffect(() => {
+    if (!EOF) {
+      if (ref.current) {
+        observer.observe(ref.current);
+      }
+      return () => {
+        observer.disconnect();
+      };
+    }
+  }, [content]);
+}
+/**
+ * Displays all the paragraphs and adds magic scroll element which is used for
+ * detecting when the user has scrolled to the bottom of the screen
+ * See: useVis
+ *
+ * @param {object} props
+ */
+export function ShowContent(props) {
+  const { content, currentPage, setCurrentPage, EOF, fetchParams } = props;
+  const scrollEle = useRef(null);
+  useVis(scrollEle, currentPage, setCurrentPage, fetchParams, content, EOF);
+  return (
+    <div>
+      <ul className="contract-paragraphs">
+        <ShowParagraphs content={content} />
+      </ul>
+      {!EOF && (
+        <Button
+          label={"Fetch more paragraphs"}
+          action={() => {
+            console.log("currentPage,", currentPage);
+            setCurrentPage(currentPage + 1);
+          }}
+        />
+      )}
+      {!EOF && <div ref={scrollEle}>---Scroll for more---</div>}
+    </div>
+  );
+}
